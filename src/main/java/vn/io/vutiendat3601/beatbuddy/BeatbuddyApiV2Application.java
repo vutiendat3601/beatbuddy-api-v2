8b388f638e04d54ec40e65b6682740fb4edd8d81
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-07-23 21:40:37 +0700

    chore: initial commit

---
+++ src/main/java/vn/io/vutiendat3601/beatbuddy/BeatbuddyApiV2Application.java
@@ -0,0 +1,11 @@
+package vn.io.vutiendat3601.beatbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatbuddyApiV2Application {
+  public static void main(String[] args) {
+    SpringApplication.run(BeatbuddyApiV2Application.class, args);
+  }
+}
